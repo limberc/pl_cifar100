93ae55693f5146024031d6d3b811a13a87ac5da0
程泽华 <chengzehua@outlook.com>
2020-11-23 14:09:58 +0800

    :sparkles: Add SEResNet.

--- models/resnet.py
+++ models/resnet.py
@@ -74,16 +74,16 @@ class ResNet(nn.Module):
 
         self.in_channels = 64
 
-        self.conv1 = nn.Sequential(
+        self.layer0 = nn.Sequential(
             nn.Conv2d(3, 64, kernel_size=3, padding=1, bias=False),
             nn.BatchNorm2d(64),
             nn.ReLU(inplace=True))
         # we use a different inputsize than the original paper
-        # so conv2_x's stride is 1
-        self.conv2_x = self._make_layer(block, 64, num_block[0], 1)
-        self.conv3_x = self._make_layer(block, 128, num_block[1], 2)
-        self.conv4_x = self._make_layer(block, 256, num_block[2], 2)
-        self.conv5_x = self._make_layer(block, 512, num_block[3], 2)
+        # so layer1's stride is 1
+        self.layer1 = self._make_layer(block, 64, num_block[0], 1)
+        self.layer2 = self._make_layer(block, 128, num_block[1], 2)
+        self.layer3 = self._make_layer(block, 256, num_block[2], 2)
+        self.layer4 = self._make_layer(block, 512, num_block[3], 2)
         self.avg_pool = nn.AdaptiveAvgPool2d((1, 1))
         self.fc = nn.Linear(512 * block.expansion, num_classes)
 
@@ -111,11 +111,11 @@ class ResNet(nn.Module):
         return nn.Sequential(*layers)
 
     def forward(self, x):
-        output = self.conv1(x)
-        output = self.conv2_x(output)
-        output = self.conv3_x(output)
-        output = self.conv4_x(output)
-        output = self.conv5_x(output)
+        output = self.layer0(x)
+        output = self.layer1(output)
+        output = self.layer2(output)
+        output = self.layer3(output)
+        output = self.layer4(output)
         output = self.avg_pool(output)
         output = output.view(output.size(0), -1)
         output = self.fc(output)
