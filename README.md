86072ed166a0f2eed254e92d82c4699c2bb58919
limberc <zehua.cheng@cs.ox.ac.uk>
2023-08-04 22:10:06 +0100

    Update.

--- README.md
+++ README.md
@@ -1,35 +1,34 @@
-# PyTorch Lightning CIFAR100
+# Benchmarks for M1 Max
 
-This is a extremely simple PyTorch Lighting repo for Computer Vision learner.
+We trained model over M1 Max and trained for **1 epoch** on image with (3, 224, 224) for 10,000 images.
 
-## Usage
+We reported the total training time (s).
 
-You can first use `python main.py -h` to get the help.
+| Model          | #parms  | Batch Size | Time (M1 Max) | Time (3090ti) |
+| -------------- | ------- | ---------- | ------------- | ------------- |
+| ResNet50       | 23.7M   | 64         |               |               |
+|                |         | 512        |               |               |
+| ViT-Base       | 86.57M  | 64         |               |               |
+|                |         | 512        |               |               |
+| ViT-Large      | 304.20M | 64         |               |               |
+|                |         | 512        |               |               |
+| SwinT-Base     | 87.76M  | 64         |               |               |
+|                |         | 512        |               |               |
+| SwinT-Large    | 196.53M | 64         |               |               |
+|                |         | 512        |               |               |
+| ConvNeXt-Base  | 88.59M  | 64         |               |               |
+|                |         | 512        |               |               |
+| ConvNeXt-Large | 197.76M | 64         |               |               |
+|                |         | 512        |               |               |
 
-You can easily change the basic settings in `default_config.yaml`. For example, the batch size and the arch (network).
+vit_large_patch14_224
 
-## Training Details
+vit_base_patch16_224
 
-I follow the hyperparameter settings in paper [Improved Regularization of Convolutional Neural Networks with Cutout](https://arxiv.org/abs/1708.04552v2), which is init lr = 0.4 divide by 5 at 60th, 120th, 160th epochs, train for 200 epochs. The batch size that we use is 512 with weight decay 5e-4, Nesterov momentum of 0.9.
+swin_base_patch4_window7_224
 
-To note that, for CIFAR10/100, the input shape is not as the same as the ImageNet. 
-So, the model would slightly different than the ImageNet version which means the stride would be different.
+swin_large_patch4_window7_224
 
-Use ImageNet model directly would face up to the accuracy lost.
+convnext_base
 
-## Results
-
-|   network   | params | VAL ACC@1 | VAL ACC@5 |
-| :---------: | :----: | :-----: | :-----: |
-|  ResNet18   | 11.2M  |  75.83  |  93.36  |
-|  ResNet34   | 21.3M  |  77.53  |  94.11  |
-|  ResNet50   | 23.7M  |  78.12  |  94.81  |
-|  ResNet101  | 42.7M  |  78.53  |  95.13  |
-|  ResNet152  | 58.3M  |  80.53  |  95.21  |
-| MobileNetv2 |   2M   |  68.25  |  90.51  |
-|  ResNeXt50  | 14.8M  |  79.54  |  95.21  |
-| SeResNet18  | 11.4M  |  76.54  |  93.55  |
-| SeResNet34  | 21.6M  |  76.13  |  93.14  |
-| SeResNet50  | 26.5M  |  77.94  |  94.55  |
-| DenseNet121 | 7.0M | 77.01 | 93.55 |
-| DenseNet161 | 26M | 78.44 | 93.96 |
+convnext_large
--- main.py
+++ main.py
@@ -7,43 +7,22 @@ import torchvision.transforms as transforms
 from pytorch_lightning import LightningDataModule, LightningModule
 from pytorch_lightning.cli import LightningCLI
 from torch.optim import SGD
-from torch.utils.data import DataLoader, Dataset
-from torchmetrics import Accuracy
-from torchvision.datasets import CIFAR10, CIFAR100
+from torch.utils.data import DataLoader
+from torchvision.datasets import FakeData
 
 
-def get_dataset(data_path, dataset):
-    if dataset == 'cifar10':
-        mean = [x / 255 for x in [125.3, 123.0, 113.9]]
-        std = [x / 255 for x in [63.0, 62.1, 66.7]]
-    elif dataset == 'cifar100':
-        mean = [x / 255 for x in [129.3, 124.1, 112.4]]
-        std = [x / 255 for x in [68.2, 65.4, 70.4]]
-    train_transform = transforms.Compose(
-        [transforms.RandomHorizontalFlip(),
-         transforms.RandomCrop(32, padding=4),
-         transforms.ToTensor(),
-         transforms.Normalize(mean, std)])
-    test_transform = transforms.Compose(
-        [transforms.ToTensor(), transforms.Normalize(mean, std)])
-    if dataset == 'cifar10':
-        train_data = CIFAR10(data_path, train=True, transform=train_transform, download=True)
-        test_data = CIFAR10(data_path, train=False, transform=test_transform, download=True)
-    elif dataset == 'cifar100':
-        train_data = CIFAR100(data_path, train=True, transform=train_transform, download=True)
-        test_data = CIFAR100(data_path, train=False, transform=test_transform, download=True)
-    return train_data, test_data
-
-
-class CIFARDataModule(LightningDataModule):
-    def __init__(self, data_path: str = './data', dataset: str = 'cifar100', batch_size: int = 512):
+class FakeDataModule(LightningDataModule):
+    def __init__(self, batch_size: int = 512):
         super().__init__()
-        self.data_path = data_path
-        self.dataset = dataset
         self.batch_size = batch_size
 
     def setup(self, stage: Optional[str] = None):
-        self.train_data, self.test_data = get_dataset(self.data_path, self.dataset)
+        self.train_data = FakeData(10000, num_classes=1000, transform=transforms.Compose([
+            transforms.ToTensor(),
+        ]))
+        self.test_data = FakeData(num_classes=1000, transform=transforms.Compose([
+            transforms.ToTensor(),
+        ]))
 
     def train_dataloader(self):
         return DataLoader(self.train_data, batch_size=self.batch_size, shuffle=True, num_workers=4, pin_memory=True)
@@ -55,7 +34,7 @@ class CIFARDataModule(LightningDataModule):
         return DataLoader(self.test_data, batch_size=self.batch_size, shuffle=False, num_workers=4, pin_memory=True)
 
 
-class CIFARLightningModel(LightningModule):
+class VisionLightningModel(LightningModule):
     # pull out resnet names from torchvision models
     MODEL_NAMES = sorted(
         name for name in models.__dict__
@@ -76,25 +55,13 @@ class CIFARLightningModel(LightningModule):
         super().__init__()
         self.save_hyperparameters()
         self.arch = arch
-        # self.pretrained = pretrained
         self.lr = lr
         self.momentum = momentum
         self.weight_decay = weight_decay
         self.data_path = data_path
         self.dataset = dataset
         self.batch_size = batch_size
-        if dataset == 'cifar10':
-            num_classes = 10
-        elif dataset == 'cifar100':
-            num_classes = 100
-        else:
-            raise ValueError("NOT SUPPORT DATASET.")
-        self.model = models.__dict__[self.arch](num_classes=num_classes)
-        self.train_datset, self.test_dataset = get_dataset(data_path, dataset)
-        self.train_dataset: Optional[Dataset] = None
-        self.eval_dataset: Optional[Dataset] = None
-        self.train_acc = Accuracy(task='multiclass', num_classes=num_classes)
-        self.eval_acc = Accuracy(task='multiclass', num_classes=num_classes)
+        self.model = models.__dict__[self.arch]()
 
     def forward(self, x):
         return self.model(x)
@@ -103,17 +70,12 @@ class CIFARLightningModel(LightningModule):
         images, target = batch
         output = self(images)
         loss_val = F.cross_entropy(output, target)
-        # update metrics
-        self.train_acc(output, target)
-        self.log("train_acc", self.train_acc, prog_bar=True)
         return loss_val
 
     def validation_step(self, batch, batch_idx):
         images, target = batch
         output = self(images)
         loss_val = F.cross_entropy(output, target)
-        self.eval_acc(output, target)
-        self.log("val_acc1", self.eval_acc, prog_bar=True)
         return loss_val
 
     def configure_optimizers(self):
@@ -122,7 +84,7 @@ class CIFARLightningModel(LightningModule):
             lr=(self.lr or self.learning_rate),
             momentum=self.momentum,
             weight_decay=self.weight_decay,
-            nesterov=True
+            nesterov=True,
         )
         scheduler = lr_scheduler.MultiStepLR(
             optimizer, milestones=[60, 120, 160], gamma=0.2
@@ -135,14 +97,16 @@ class CIFARLightningModel(LightningModule):
 
 if __name__ == '__main__':
     LightningCLI(
-        CIFARLightningModel, CIFARDataModule,
+        VisionLightningModel, FakeDataModule,
         seed_everything_default=42,
         save_config_callback=None,
         trainer_defaults={
-            "max_epochs": 200,
-            "accelerator": "auto",
-            "strategy": "ddp_find_unused_parameters_false",
-            "devices": 2,
+            "max_epochs": 1,
+            "accelerator": "mps",
+            "strategy": "auto",
+            "profiler": "simple",
+            "num_sanity_val_steps": 0,
+            "devices": -1,
             "benchmark": True,
         }
     )
